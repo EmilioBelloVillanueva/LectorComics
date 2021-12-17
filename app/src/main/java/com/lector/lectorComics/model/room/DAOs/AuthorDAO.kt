51795eb2ea865497824165f2a8c650525c0548e9
Emilio Bello <emilio.bello@imbox.me>
2021-12-17 13:49:41 +0100

    #10 Creado AuthorDAO con room

---
+++ app/src/main/java/com/lector/lectorComics/model/room/DAOs/AuthorDAO.kt
@@ -0,0 +1,29 @@
+package com.lector.lectorComics.model.room.DAOs
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.lector.lectorComics.model.room.entitiesRoom.AuthorRoom
+
+@Dao
+
+interface AuthorDAO {
+    @Query("SELECT * FROM author")
+    fun getAll(): List<AuthorRoom>
+
+    @Query("SELECT * FROM author WHERE id IN (:ids)")
+    fun loadAllByIds(ids: IntArray): List<AuthorRoom>
+
+    @Query("SELECT * FROM author WHERE id IN (:id) LIMIT 1")
+    fun loadById(id: Int): AuthorRoom
+
+    @Query("SELECT * FROM author WHERE name LIKE :name")
+    fun findByName(name: String): List<AuthorRoom>
+
+    @Insert
+    fun insertAll(vararg authors: AuthorRoom)
+
+    @Delete
+    fun delete(author: AuthorRoom)
+}
--- app/src/main/java/com/lector/lectorComics/model/room/entitiesRoom/AuthorRoom.kt
+++ app/src/main/java/com/lector/lectorComics/model/room/entitiesRoom/AuthorRoom.kt
@@ -3,7 +3,7 @@ package com.lector.lectorComics.model.room.entitiesRoom
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "author")
 data class AuthorRoom(
     @PrimaryKey val id: Int,
     val name: String,
